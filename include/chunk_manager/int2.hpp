39b460539f9de4178572deec59fcb0a49008d386
Francois Sestier <francois.sestier@gmail.com>
2021-08-03 23:06:41 +0200

    basic int2 wrapped subject to change

---
+++ include/chunk_manager/int2.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <format>
+
+namespace ckm {
+
+    struct int2 {
+        int x;
+        int y;
+    };
+
+    namespace details {
+
+        auto to_string(int x, int y) {
+            return std::format("x : {},  y : {}", x, y);
+        }
+
+        auto to_string(const int2& val) { to_string(val.x, val.y); }
+
+    }  // namespace details
+
+
+}  // namespace ckm
