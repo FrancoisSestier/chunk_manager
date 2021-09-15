a0eaaabc1163aae00b60ab7cd76cba1c38c8601b
Francois Sestier <francois.sestier@gmail.com>
2021-09-15 12:47:01 +0100

    inlining to_string def for int2 to avoid breaking  the one definition rule

--- include/chunk_manager/int2.hpp
+++ include/chunk_manager/int2.hpp
@@ -10,11 +10,11 @@ namespace ckm {
 
     namespace details {
 
-        auto to_string(int x, int y) {
+        inline static auto to_string(int x, int y) {
             return fmt::format("x : {},  y : {}", x, y);
         }
 
-        auto to_string(const int2& val) { return to_string(val.x, val.y); }
+        inline static auto to_string(const int2& val) { return to_string(val.x, val.y); }
 
     }  // namespace details
 
